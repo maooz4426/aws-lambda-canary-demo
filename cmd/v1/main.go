6c1ca8bdf077dde52f985c1d35e5d50bf98b186d
MAOZ <maooz4426@gmail.com>
2026-06-16 00:54:56 +0900

    zip形式を追加

---
+++ cmd/v1/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/aws-lambda-canary-demo/internal/hello"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	helloService := hello.NewHelloService("v1")
+
+	lambda.Start(helloService.SayHello)
+}
